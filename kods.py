3b9156525f2997c227d26ab13fc3987cf1f3b19c
nikolashenrijskunce <144266000+nikolashenrijskunce@users.noreply.github.com>
2024-01-15 19:43:48 +0200

    Update kods.py
    Tiek uzrakstīts koda pamats pēc kura tiks aprakstīta tālāka darbība.

--- kods.py
+++ kods.py
@@ -1,1 +1,20 @@
+# Importē visas nepieciešamās bibliotēkas. Ja bibliotēkas nestrādā, tad ir jāizpild kods "pip install selenium".
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
 
+# Nodefinē mainīgo driver, lai programma spētu kontrolēt pārlūkprogrammu Google Chrome.
+service = Service()
+option = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service = service, options = option)
+
+
+
+# Tiek definēta web adrese, kur tiek meklēta informācija par filmas reģionu
+url = "https://www.google.com/"
+
+# Atver mājaslapu, uz kuru aizved adrese.
+driver.get(url)
+time.sleep(0.5)
