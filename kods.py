e591f9803c5b762036adb7347ef2da2da972a97d
nikolashenrijskunce <144266000+nikolashenrijskunce@users.noreply.github.com>
2024-01-16 15:40:34 +0200

    Update kods.py
    Kodā pievienotas darbības ar Google Chrome interneta pārlūku, lai varētu iegūt Netflix saites adresi uz noteikto filmu.

--- kods.py
+++ kods.py
@@ -5,6 +5,10 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 
+# Lietotājam dod ievadīt meklētās filmas nosaukumu.
+print("Filmas nosaukums: ")
+film = input()
+
 # Nodefinē mainīgo driver, lai programma spētu kontrolēt pārlūkprogrammu Google Chrome.
 service = Service()
 option = webdriver.ChromeOptions()
@@ -12,16 +16,28 @@ driver = webdriver.Chrome(service = service, options = option)
 
 
 
-# Tiek definēta web adrese, kur tiek meklēta informācija par filmas reģionu
+# Tiek definēta web adrese, kur tiek meklēta informācija par filmas reģionu.
 url = "https://www.google.com/"
 
 # Atver mājaslapu, uz kuru aizved adrese.
 driver.get(url)
 time.sleep(0.5)
 
-# Noraida Google sīkfailus
+# Noraida Google sīkfailus.
 decline_cookies = driver.find_element(By.ID, "W0wltc")
 decline_cookies.click()
 time.sleep(0.1)
 
+# Ievada meklējamo frāzi logā un to sameklē ar Google.
+search_box = driver.find_element(By.ID, "APjFqb")
+search = film + " netflix"
+search_box.send_keys(search)
+time.sleep(0.5)
+enter = driver.find_element(By.NAME, "btnK")
+enter.click()
+time.sleep(1)
 
+# Atrod filmas netflix saites adresi un aiztaisa Google Chrome.
+find = driver.find_element(By.XPATH, "//a[@jsname='UWckNb']")
+link = find.get_attribute("href")
+driver.close()
