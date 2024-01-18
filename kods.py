9d4d3a348a88767887aa81a53d6d45e7f2c645cb
nikolashenrijskunce <144266000+nikolashenrijskunce@users.noreply.github.com>
2024-01-18 17:44:51 +0200

    Update kods.py
    Uzlabojumi kodā, kas novērš kļūdas rašanos ierakstot neeksistējošu filmas nosaukumu.

--- kods.py
+++ kods.py
@@ -3,6 +3,7 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 from openpyxl import Workbook, load_workbook
 
@@ -32,10 +33,7 @@ time.sleep(0.1)
 # Ievada meklējamo frāzi logā un to sameklē ar Google.
 search_box = driver.find_element(By.ID, "APjFqb")
 search = film + " netflix"
-search_box.send_keys(search)
-time.sleep(0.5)
-enter = driver.find_element(By.NAME, "btnK")
-enter.click()
+search_box.send_keys(search, Keys.ENTER)
 time.sleep(1)
 
 # Atrod filmas netflix saites adresi un aiztaisa Google Chrome.
@@ -50,7 +48,7 @@ page_name = b[12] + b[13] + b[14] + b[15] + b[16] + b[17] + b[18]
 region = b[24] + b[25]
 
 # Izvelk Netflix reģiona kodu no saites adreses un atrod atbilstošo valsti Excel tabulā. Izvada rezultātu.
-if (page_name == "netflix" and (region + b[26] + b[27] + b[28] + b[29]) != "search"):
+if (page_name == "netflix" and len(b) > 29 and (region + b[26] + b[27] + b[28] + b[29]) != "search"):
   if ((region + b[26] + b[27] + b[28]) == "title"):
     print("Šī filma ir pieejama Jūsu pašreizējā reģionā.")
   else:
@@ -61,5 +59,8 @@ if (page_name == "netflix" and (region + b[26] + b[27] + b[28] + b[29]) != "sear
       country_name=(ws['a' + str(row)].value)
       if (country_code == region):
         print("Filma ir pieejama šādā reģionā: " + country_name)
+        row = row - 1
+      if (row == 250):
+        print("Šī filma nav pieejama vietnē Netflix.")
 else:
   print("Šī filma nav pieejama vietnē Netflix.")
