9416faa1ddb233cf303048c5b76b9bb83cd7ac99
nikolashenrijskunce <144266000+nikolashenrijskunce@users.noreply.github.com>
2024-01-18 17:58:14 +0200

    Update kods.py
    Uzlabojumi, lai novērstu kļūdas rašanos.

--- kods.py
+++ kods.py
@@ -44,23 +44,26 @@ driver.close()
 # Definē mainīgos, kādi tiks izmantoti, lai atrastu valsti, kurā ir filma ir pieejama.
 b = []
 b = link 
+if (len(b) > 29):
 page_name = b[12] + b[13] + b[14] + b[15] + b[16] + b[17] + b[18] 
 region = b[24] + b[25]
 
 # Izvelk Netflix reģiona kodu no saites adreses un atrod atbilstošo valsti Excel tabulā. Izvada rezultātu.
-if (page_name == "netflix" and len(b) > 29 and (region + b[26] + b[27] + b[28] + b[29]) != "search"):
-  if ((region + b[26] + b[27] + b[28]) == "title"):
-    print("Šī filma ir pieejama Jūsu pašreizējā reģionā.")
+  if (page_name == "netflix" and (region + b[26] + b[27] + b[28] + b[29]) != "search"):
+    if ((region + b[26] + b[27] + b[28]) == "title"):
+      print("Šī filma ir pieejama Jūsu pašreizējā reģionā.")
+    else:
+      wb = load_workbook('CountryCodes.xlsx')
+      ws = wb.active
+      for row in range (1, 250):
+        country_code=(ws['b' + str(row)].value)
+        country_name=(ws['a' + str(row)].value)
+        if (country_code == region):
+          print("Filma ir pieejama šādā reģionā: " + country_name)
+          row = row - 1
+        if (row == 250):
+          print("Šī filma nav pieejama vietnē Netflix.")
   else:
-    wb = load_workbook('CountryCodes.xlsx')
-    ws = wb.active
-    for row in range (1, 250):
-      country_code=(ws['b' + str(row)].value)
-      country_name=(ws['a' + str(row)].value)
-      if (country_code == region):
-        print("Filma ir pieejama šādā reģionā: " + country_name)
-        row = row - 1
-      if (row == 250):
-        print("Šī filma nav pieejama vietnē Netflix.")
+    print("Šī filma nav pieejama vietnē Netflix.")
 else:
   print("Šī filma nav pieejama vietnē Netflix.")
